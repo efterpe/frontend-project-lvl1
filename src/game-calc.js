0b3ad36f50598d1dbf7e1bd086f81e01bec1edbb
Natalia Markelova <natalia@markelova.online>
2021-03-18 22:17:40 +0300

    Step 6.1

--- src/game-calc.js
+++ src/game-calc.js
@@ -1,17 +1,17 @@
 import readlineSync from 'readline-sync';
 import { name } from '../src/cli.js'
 import {hello} from '../bin/brain-games.js'
-import { CreateRandom, Random, check } from './game-even'
+import { CreateRandom, Random, check } from './game-even.js'
 
-export const isCalc = (number1, number2, oper) => {
-    if (oper === '+') {
+export const isCalc = (number1, number2, operation) => {
+    if (operation === '+') {
         return number1 + number2;
     } else
-    if (oper === '-') {
+    if (operation === '-') {
         return number1 - number2;
     } else
-    if (oper === '*') {
-        return number1 - number2;
+    if (operation === '*') {
+        return number1 * number2;
     }
 };
 
@@ -20,15 +20,15 @@ export const calc = () => {
   console.log('What is the result of the expression?');
   let numbers1 = CreateRandom();  
   let numbers2 = CreateRandom();  
-  let operations = ['+', '-', '*'];
-  for(const i = 0; i < 3; i += 1) {
-      const j = Random(3);
-        console.log('Question: ' + numbers1[i] + j + numbers2[i]);
-        let useranswer = readlineSync.question('Your answer:');
-         if ((check(isEven(number1[i], number2[i], j), useranswer)) == false) {
-           break;
-          } 
-          console.log('Congratulations, ' + name);
-         }
+  let  operations = ['+', '-', '*'];
+  for(const i = 0; i < numbers1.length; i += 1) {
+    let j = Random(3);
+    console.log('Question: ' + numbers1[i] + ' ' + operations[j] +  ' ' + numbers2[i]);
+    let useranswer = readlineSync.question('Your answer: ');
+    if ((check(isCalc( numbers1[i], numbers2[i], operations[j]) , Number(useranswer))) ===  false) {
+        break;
+        } 
+        console.log('Congratulations, ' + name);
+        }
     };
     
