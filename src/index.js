095045d190169cf9b74ec12cdff41a1814ff8741
Natalia Markelova <natalia@markelova.online>
2021-05-16 22:26:42 +0300

    fix engine

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 import readlineSync from 'readline-sync';
 
-export default function engine(condition, task) {
+export default function engine(condition, getData) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
   console.log(condition);
 
-  let round = 0;
-
   for (let i = 0; i < 3; i += 1) {
-    const getData = task();
-    const [generatedAnswer, generatedQuestion] = getData;
+    const [generatedAnswer, generatedQuestion] = getData();
     console.log(`Question: ${generatedQuestion}`);
     const userAnswer = readlineSync.question('Your answer:');
     if (userAnswer === generatedAnswer) {
       console.log('Correct!');
-      round += 1;
     } else {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${generatedAnswer}.`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
   }
-
-  if (round === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  console.log(`Congratulations, ${name}!`);
 }
