d790b3df343daf8bbbc1f55b56ff98f856d620a2
Natalia Markelova <natalia@markelova.online>
2021-05-26 14:38:36 +0300

    progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,12 +4,12 @@ import generateNumbers from '../generateNumbers.js';
 const condition = 'What number is missing in the progression?';
 
 const createNumbers = () => {
-  const firstnumber = generateNumbers(0, 100);
+  const firstnumber = generateNumbers(1, 100);
   const step = generateNumbers(1, 10);
   const hide = generateNumbers(1, 100);
   let numbers = [];
 
-  for (let i = 1; i <= 10; i += 1) {
+  for (let i = 1; i <= 20; i += 1) {
     numbers.push(firstnumber + (step * i));
   }
   const answer = String(numbers[hide]);
